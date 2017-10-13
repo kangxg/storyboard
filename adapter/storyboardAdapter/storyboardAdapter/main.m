53171b1a7c7d221c743f25c027e5244ec75b10ac
kangxiaoguang <kangxiaoguang@etiantian.com>
2017-10-13 11:25:38 +0800

    创建工程

---
+++ adapter/storyboardAdapter/storyboardAdapter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  storyboardAdapter
+//
+//  Created by kangxg on 2017/10/13.
+//  Copyright © 2017年 zy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
